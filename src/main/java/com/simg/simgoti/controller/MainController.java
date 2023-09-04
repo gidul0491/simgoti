1959d8f8f83e7aea400a062275b0cbaa13323015
gidul <quack0258@gmail.com>
2023-09-04 17:18:57 +0900

    no message

--- src/main/java/com/simg/simgoti/controller/MainController.java
+++ src/main/java/com/simg/simgoti/controller/MainController.java
@@ -1,12 +1,13 @@
 package com.simg.simgoti.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping(value = "/")
     public String index() throws Exception {
         return "index.html";
     }
